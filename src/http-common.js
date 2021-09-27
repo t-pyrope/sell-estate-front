8383e2c3d55d3b5eb94e07941b2080af7102b057
badylbek <Begaiym.Adylbek.Kyzy@teskalabs.com>
2021-09-27 21:00:22 +0200

    Add (commented) code when server is in MongoDB

--- src/http-common.js
+++ src/http-common.js
@@ -1,5 +1,14 @@
 import axios from "axios";
 
+// when server is in MongoDB
+// export default axios.create({
+//     baseURL: "https://eu-central-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/sell-estate-ezpbb/service/lead/incoming_webhook/",
+//     headers: {
+//         "Content-type": "application/json"
+//     }
+// });
+
+// when server is local
 export default axios.create({
     baseURL: "http://localhost:5000/lead",
     headers: {
--- src/services/estates.js
+++ src/services/estates.js
@@ -1,5 +1,13 @@
 import http from '../http-common';
 
+// when server is in MongoDB
+// class EstatesDataService {
+//     createEstate(data){
+//         return http.put("lead", data)
+//     }
+// }
+
+// when server is on local machine
 class EstatesDataService {
     createEstate(data){
         return http.put("/", data)
